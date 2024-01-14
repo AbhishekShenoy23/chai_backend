43f0a07c3b2e7ede1855f2f8e3a23e825cea3db8
AbhishekShenoy23 <abhishekshenoy90@gmail.com>
2024-01-14 16:21:30 +0530

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,34 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandlers } from "../utils/asyncHandlers.js";
+import { User } from "../models/user.models.js";
 
 
 const userRegister = asyncHandlers(async(req,res)=>{
 
-res.status(200).json({message:"ok"})
+// Fetch the data and file information from frontend.
+// check if user already exists.  using email and username as these are unique
+// validate if amy fields coming in are empty
+// Fetch file from front end store it in tem loc.
+// get the path of temp loc
+// move the file to cloudinary and get the reponse url.
+// check for success or failure while uploading avatar and cover image
+//create the user 
+// send the response to the frontend.
+
+
+const {email,username,fullName,password} = req.body
+
+if([email,username,fullName,password].some((field)=> field?.trim()=== ""))
+{
+    throw new ApiError(400,"All fields are Mandatory")
+}
+
+const existedUser = await User.findOne({$or:[{email},{username}]})
+
+if(existedUser) 
+throw new ApiError(409,"User Already Exists")
+
+
 
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router} from "express";
 import { userRegister } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router= Router()
 
-
-router.route("/register").post(userRegister)
+// calling the multer middleware in userroutes
+router.route("/register").post(
+    upload.fields(
+        [
+            {name:"avatar",maxCount:1},
+            {name:"coverImage",maxCount:1}
+        ])
+ ,userRegister)
 
 export default router
